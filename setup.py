3379a2c181ebb401cd338759f3b769aa9d986a78
Ryan Lovett <rylo@berkeley.edu>
2022-12-02 15:38:57 -0800

    Base project name on class name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jupyterhub-canvasauthenticator',
+    name='jupyterhub-canvasoauthenticator',
     version='0.1dev',
     python_requires='>=3.5',
     packages=find_packages(),
