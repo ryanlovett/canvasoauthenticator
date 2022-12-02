fc509fa898734c5548782239134bcdf5d12fe2bd
Ryan Lovett <rylo@berkeley.edu>
2022-12-02 15:35:16 -0800

    Reformat with `black`.

--- canvasauthenticator/__init__.py
+++ canvasauthenticator/__init__.py
@@ -3,6 +3,7 @@ import aiohttp
 from traitlets import List, Unicode, default
 from oauthenticator.generic import GenericOAuthenticator
 
+
 class CanvasOAuthenticator(GenericOAuthenticator):
     """
     Canvas OAuth2 based authenticator for JupyterHub.
@@ -12,7 +13,7 @@ class CanvasOAuthenticator(GenericOAuthenticator):
     """
 
     strip_email_domain = Unicode(
-        '',
+        "",
         config=True,
         help="""
         Strip this domain from user emails when making their JupyterHub user name.
@@ -24,21 +25,21 @@ class CanvasOAuthenticator(GenericOAuthenticator):
 
         By default, *no* domain stripping is performed, and the JupyterHub username
         is the primary email of the canvas user.
-        """
+        """,
     )
 
     canvas_url = Unicode(
-        '',
+        "",
         config=True,
         help="""
         URL to canvas installation to use for authentication.
 
         Must have a trailing slash
-        """
+        """,
     )
 
     canvas_course_key = Unicode(
-        '',
+        "",
         config=True,
         help="""
         Key to lookup course identifier from Canvas course data.
@@ -49,10 +50,10 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         id examples: 12345, 23456
         sis_course_id examples: CRS:MATH-98-2021-C, CRS:CHEM-1A-2021-D, CRS:PHYSICS-77-2022-C
         course_code examples: "Math 98", "Chem 1A Fall 2021", "PHYSICS 77-LEC-001"
-        """
+        """,
     )
 
-    @default('canvas_course_key')
+    @default("canvas_course_key")
     def _default_canvas_course_key(self):
         """
         The default is 'id', an integer which doesn't convey anything about the
@@ -66,27 +67,29 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         by nicknames, and the user won't be excluded from reading it, but it is
         not garanteed to be unique.
         """
-        return 'id'
+        return "id"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         if not self.canvas_url:
-            raise ValueError('c.CanvasOAuthenticator.canvas_url must be set')
+            raise ValueError("c.CanvasOAuthenticator.canvas_url must be set")
 
         # canvas_url must have a trailing slash
-        if self.canvas_url[-1] != '/':
-            raise ValueError('c.CanvasOAuthenticator.canvas_url must have a trailing slash')
+        if self.canvas_url[-1] != "/":
+            raise ValueError(
+                "c.CanvasOAuthenticator.canvas_url must have a trailing slash"
+            )
 
-        self.token_url = f'{self.canvas_url}login/oauth2/token'
-        self.userdata_url = f'{self.canvas_url}api/v1/users/self/profile'
+        self.token_url = f"{self.canvas_url}login/oauth2/token"
+        self.userdata_url = f"{self.canvas_url}api/v1/users/self/profile"
 
         self.extra_params = {
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
             # We set replace_tokens=1 to prevent tokens from accumulating.
             # https://github.com/instructure/canvas-lms/blob/release/2022-08-03.12/spec/controllers/oauth2_provider_controller_spec.rb#L520
-            'replace_tokens': 1,
+            "replace_tokens": 1,
         }
 
     async def get_canvas_items(self, token, url):
@@ -94,16 +97,18 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         Get paginated items from Canvas.
         https://canvas.instructure.com/doc/api/file.pagination.html
         """
-        headers = dict(Authorization = f"Bearer {token}")
+        headers = dict(Authorization=f"Bearer {token}")
         data = []
 
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=headers, params=self.extra_params) as r:
                 if r.status != 200:
-                    raise Exception(f"error fetching items for {self.username}: {url} -- {r.status} -- {r.text()}")
+                    raise Exception(
+                        f"error fetching items for {self.username}: {url} -- {r.status} -- {r.text()}"
+                    )
                 data = await r.json()
-                if 'next' in r.links.keys():
-                    url = r.links['next']['url']
+                if "next" in r.links.keys():
+                    url = r.links["next"]["url"]
                     data += await self.get_canvas_items(token, url)
 
         return data
@@ -122,15 +127,15 @@ class CanvasOAuthenticator(GenericOAuthenticator):
 
     def format_group(self, course_identifier, enrollment_type):
         if enrollment_type is None:
-            return f'canvas::{course_identifier}'
+            return f"canvas::{course_identifier}"
         else:
-            return f'canvas::{course_identifier}::{enrollment_type}'
+            return f"canvas::{course_identifier}::{enrollment_type}"
 
     def extract_course_groups(self, courses):
-        '''
+        """
         Extract course identifiers for each course the user is enrolled in
         and format them as group names.
-        '''
+        """
         groups = []
 
         for course in courses:
@@ -142,9 +147,7 @@ class CanvasOAuthenticator(GenericOAuthenticator):
             # https://canvas.instructure.com/doc/api/courses.html#method.courses.index
             # there may be multiple enrollments per course
             enrollment_types = list(
-                map(
-                    lambda x: x.get('type', None), course.get('enrollments', [])
-                )
+                map(lambda x: x.get("type", None), course.get("enrollments", []))
             )
 
             for enrollment_type in enrollment_types:
@@ -157,9 +160,9 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         Augment base user auth info with course info
         """
         user = await super().authenticate(handler, data)
-        courses = await self.get_courses(user['auth_state']['access_token'])
-        user['auth_state']['courses'] = courses
-        #user['groups'] = self.extract_course_groups(courses)
+        courses = await self.get_courses(user["auth_state"]["access_token"])
+        user["auth_state"]["courses"] = courses
+        # user['groups'] = self.extract_course_groups(courses)
         return user
 
     def normalize_username(self, username):
@@ -167,8 +170,8 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         # To make life easier & match usernames with existing users who were
         # created with google auth, we want to strip the domain name. If not,
         # we use the full email as the official user name
-        if self.strip_email_domain and username.endswith('@' + self.strip_email_domain):
-            return username.split('@')[0]
+        if self.strip_email_domain and username.endswith("@" + self.strip_email_domain):
+            return username.split("@")[0]
         return username
 
     async def pre_spawn_start(self, user, spawner):
@@ -176,10 +179,12 @@ class CanvasOAuthenticator(GenericOAuthenticator):
         auth_state = yield user.get_auth_state()
         if not auth_state:
             return
-        if 'access_token' in auth_state:
-            spawner.environment["OAUTH2_ACCESS_TOKEN"] = auth_state['access_token']
+        if "access_token" in auth_state:
+            spawner.environment["OAUTH2_ACCESS_TOKEN"] = auth_state["access_token"]
         # others are lti_user_id, id, integration_id
-        if 'oauth_user' in auth_state:
-            for k in ['login_id', 'name', 'sortable_name', 'primary_email']:
-                if k in auth_state['oauth_user']:
-                    spawner.environment[f"OAUTH2_{k.upper()}"] = auth_state['oauth_user'][k]
+        if "oauth_user" in auth_state:
+            for k in ["login_id", "name", "sortable_name", "primary_email"]:
+                if k in auth_state["oauth_user"]:
+                    spawner.environment[f"OAUTH2_{k.upper()}"] = auth_state[
+                        "oauth_user"
+                    ][k]
