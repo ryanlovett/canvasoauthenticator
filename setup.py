f3a7b95f8aab56b21d1ac94d739639076571758c
Ryan Lovett <rylo@berkeley.edu>
2023-03-16 20:27:47 -0700

    Bump to v0.1.
    We've been using this in production for a while so let's remove the "dev" suffix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-canvasoauthenticator',
-    version='0.1dev',
+    version='0.1',
     python_requires='>=3.5',
     packages=find_packages(),
     install_requires=[
